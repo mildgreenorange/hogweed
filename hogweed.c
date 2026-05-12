2eefa4dc1384c318a9732d2a566407ca68cc14c8
szStupidGuy <golfball2763@proton.me>
2026-05-12 18:31:58 +0100

    Update hogweed.c
    This, kids, is why you never commit without debugging.

--- hogweed.c
+++ hogweed.c
@@ -1,42 +1,45 @@
 #include <string.h>
 #include <math.h>
 
-char* hogweedXor(char chMessage, int iCode) {
-	for (int i = 0; chMessage[i] != '\0'; i += 1) {
-		chMessage[i] = chMessage[i] ^ iCode;
-	}
-	
-	return chMessage;
+char* hogweedXor(char *chMessage, int iCode) {
+    for (int i = 0; chMessage[i] != '\0'; i++) {
+        chMessage[i] = chMessage[i] ^ iCode;
+    }
+    return chMessage;
 }
 
-char* hogweedShift(char chMessage, int iCode) {
-	for (int i = 0; chMessage[i] != '\0'; i += 1) {
-		j = sizeof(iCode);
-	
-		if (i % 2 == 0) {
-			chMessage[i] += iCode[j % i];
-		} else {
-			chMessage[i] += -iCode[j % i];
-		}
-	}
-	
-	return chMessage;
+char* hogweedShift(char *chMessage, int iCode) {
+    unsigned char *bytes = (unsigned char*)&iCode;
+    int j = sizeof(iCode);
+
+    for (int i = 0; chMessage[i] != '\0'; i++) {
+        int idx = (i == 0) ? 0 : (j % i);
+
+        if (i % 2 == 0) {
+            chMessage[i] += bytes[idx];
+        } else {
+            chMessage[i] -= bytes[idx];
+        }
+    }
+    return chMessage;
 }
 
-char* hogweedSpaceless(char chMessage) {
-	for (int i = 0; chMessage[i] != '\0'; i += 1) {
-		hogweedAdvance(chMessage, i);
-	}
-	
-	return chMessage;
+char* hogweedSpaceless(char *chMessage) {
+    for (int i = 0; chMessage[i] != '\0'; ) {
+        if (chMessage[i] == ' ') {
+            hogweedAdvance(chMessage, i);
+        } else {
+            i++;
+        }
+    }
+    return chMessage;
 }
 
-char* hogweedAdvance(char chMessage, int i) {
-	if (chMessage[i] == ' ') {
-		for (int j = i; chMessage[j] != '\0'; j += 1) {
-			chMessage[j] = chMessage[j + 1];
-		}
-	}
-	
-	return chMessage;
+char* hogweedAdvance(char *chMessage, int i) {
+    if (chMessage[i] == ' ') {
+        for (int j = i; chMessage[j] != '\0'; j++) {
+            chMessage[j] = chMessage[j + 1];
+        }
+    }
+    return chMessage;
 }
